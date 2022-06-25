74e2a40481d5e5af6aef92739ef66162231ab6b2
zaqNitori <zaq81009@gmail.com>
2022-06-25 19:01:28 +0800

    Finish merge parsing into final project

--- CG_LAB/ObjParser.cpp
+++ CG_LAB/ObjParser.cpp
@@ -1,27 +1,33 @@
-#include <iostream>
-#include <fstream>
-#include <string>
-
-using namespace std;
-
-
-int main(void)
-{
-	ifstream ifs("../../final/octahedron.obj", ifstream::in);
-	string str;
-	float f[3];
-	int idx;
-
-	while (ifs >> str)
-	{
-		if (str == "f" || str == "v")
-		{
-			idx = 0;
-			while (ifs >> f[idx++] && idx < 3);
-			cout << f[0] << " " << f[1] << " " << f[2] << endl;
-		}
-
-	}
-	ifs.close();
-	return 0;
-}
+//#include <iostream>
+//#include <fstream>
+//#include <string>
+//
+//using namespace std;
+//
+//typedef struct
+//{
+//    float r, g, b;
+//	float x, y, z;
+//}Vertex;
+//
+//int main(void)
+//{
+//	ifstream ifs;
+//	string str;
+//	float f[3];
+//	int idx;
+//
+//	ifs.open("../../final/octahedron.obj", ifstream::in);
+//	while (ifs >> str)
+//	{
+//		if (str == "f" || str == "v")
+//		{
+//			idx = 0;
+//			while (ifs >> f[idx++] && idx < 3);
+//			cout << f[0] << " " << f[1] << " " << f[2] << endl;
+//		}
+//
+//	}
+//	ifs.close();
+//	return 0;
+//}
